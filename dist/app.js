bd024dbe26f15cbe6661f851adbb4efc96fdba57
Kaleb Dykema <dykemakaleb@gmail.com>
2021-06-21 23:05:41 -0500

    Messed with external templates

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 // // START: sets the inner HTML of an h1 to a Mustache template
+// // <h1 class="title"></h1>
 // let title = document.getElementById('title')
 // let view = {
 //   name: 'Kaleb',
@@ -12,6 +13,10 @@
 
 
 // // TEMPLATES: pulls the template from a script in the index.html with the type of x-tmpl-mustache, then renders it with a view and outputs it to the target's innerHTML
+// // <div id="target">Loading...</div>
+// //   <script id="template" type="x-tmpl-mustache">
+// //   Hello {{name}}!
+// // </script>
 // document.onload = renderHello()
 
 // function renderHello() {
@@ -23,4 +28,16 @@
 
 
 
-// 
+// // LOADING EXTERNAL TEMPLATES: same as above, but with templates stored in individual files and done asynchronously with fetch 
+// // HTML: <div id="target">Loading...</div>
+// // template.mustache: Hello {{name}}!
+// document.onload = renderHello()
+
+// function renderHello(){
+//   fetch('./template.mustache')
+//     .then((response => response.text()))
+//     .then((template) => {
+//       let output = Mustache.render(template, {name: 'Kaleb'})
+//       document.getElementById('target').innerHTML = output
+//     })
+// }
